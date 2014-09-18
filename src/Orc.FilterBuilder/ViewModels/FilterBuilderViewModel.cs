4ea058e3686910e88f3e3ff382d608a638576b78
Geert van Horrik <geert@catenalogic.com>
2014-09-18 19:23:58 +0200

    Fixed possible null reference exception

--- src/Orc.FilterBuilder/ViewModels/FilterBuilderViewModel.cs
+++ src/Orc.FilterBuilder/ViewModels/FilterBuilderViewModel.cs
@@ -118,7 +118,7 @@ namespace Orc.FilterBuilder.ViewModels
             {
                 _targetType = CollectionHelper.GetTargetType(RawCollection);
                 newSchemes.AddRange((from scheme in _filterSchemes.Schemes
-                                     where _targetType.IsAssignableFromEx(scheme.TargetType)
+                                     where scheme.TargetType != null && _targetType.IsAssignableFromEx(scheme.TargetType)
                                      select scheme));
             }
 
