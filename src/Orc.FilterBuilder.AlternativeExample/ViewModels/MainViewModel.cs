8d3c925c284b6ee479e638511e3b97c3af07b725
auz34 <andrewzavgorodniy@gmail.com>
2015-02-15 22:52:25 -0800

    Add ui to present results of filtering in the alternative example

--- src/Orc.FilterBuilder.AlternativeExample/ViewModels/MainViewModel.cs
+++ src/Orc.FilterBuilder.AlternativeExample/ViewModels/MainViewModel.cs
@@ -5,19 +5,26 @@ using System.Text;
 
 namespace Orc.FilterBuilder.AlternativeExample.ViewModels
 {
+    using System.Threading.Tasks;
     using Catel;
     using Catel.MVVM;
+    using FilterBuilder.Services;
     using Services;
 
     public class MainViewModel: ViewModelBase
     {
         private IDataProvider _dataProvider;
 
-        public MainViewModel(IDataProvider dataProvider)
+        private IFilterService _filterService;
+
+        public MainViewModel(IDataProvider dataProvider, IFilterService filterService)
         {
             Argument.IsNotNull(() => dataProvider);
 
             _dataProvider = dataProvider;
+            _filterService = filterService;
+
+            FilteredCollection = new List<object>();
         }
 
         public IEnumerable<object> OriginalCollection
@@ -26,6 +33,26 @@ namespace Orc.FilterBuilder.AlternativeExample.ViewModels
             {
                 return _dataProvider.GetData();
             }
+        }
+
+        public IEnumerable<object> FilteredCollection { get; private set; }
+
+        protected override Task Initialize()
+        {
+            _filterService.SelectedFilterChanged += OnSelectedFilterChanged;
+            return base.Initialize();
+        }
+
+        protected override Task Close()
+        {
+            _filterService.SelectedFilterChanged -= OnSelectedFilterChanged;
+            return base.Close();
+        }
+
+        private void OnSelectedFilterChanged(object sender, EventArgs e)
+        {
+            FilteredCollection = _filterService.FilterCollection(_filterService.SelectedFilter, OriginalCollection);
+            RaisePropertyChanged(() => FilteredCollection);
         }
     }
 }
