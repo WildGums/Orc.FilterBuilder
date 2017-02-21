e9f964add0b0d4d7963b599ee61c2e401cd031e7
Vladimir <work.lvg@gmail.com>
2017-02-21 10:09:34 +0300

    ORCOMP-295 add runtime metadata tests

---
+++ src/Orc.FilterBuilder.Tests/Orc.FilterBuilder.Tests.Shared/RuntimeMetadataFacts/RuntimeMetadataFacts.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RuntimeMetadataFacts.cs" company="WildGums">
+//   Copyright (c) 2008 - 2017 WildGums. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Orc.FilterBuilder.Tests
+{
+    using System.Collections.Generic;
+    using Catel.IoC;
+    using FilterBuilder.Services;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class RuntimeMetadataFacts
+    {
+        [Test]
+        public void CorrectlyFilterEntitiesWithRuntimeMetadata()
+        {
+            var serviceLocator = ServiceLocator.Default;
+
+            var typeFactory = serviceLocator.ResolveType<ITypeFactory>();
+            
+            var filterScheme = FilterSchemeInitializationHelper.GetTestFilterScheme();
+            var initialCollection = TestDataProvider.GetInitialCollection();
+            var resultList = new List<TestFilterRuntimeModel>();
+
+            var filterService = typeFactory.CreateInstanceWithParametersAndAutoCompletion<FilterService>();
+            filterService.FilterCollection(filterScheme, initialCollection, resultList);
+
+            Assert.AreEqual(resultList.Count, 1);
+            Assert.AreEqual(resultList[0].Attributes[AttributeTypeNames.StringAttribute].Value, "one");
+        }
+    }
+}
