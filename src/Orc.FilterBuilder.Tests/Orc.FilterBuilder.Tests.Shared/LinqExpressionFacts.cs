8c3b93cb387b03d3917c64f31bd708a3e31c4360
YGD\S_ILYIN <s.ilyin@ygd.gazprom.ru>
2017-08-16 16:00:02 +0500

    Added Extention to Create Linq Expression

---
+++ src/Orc.FilterBuilder.Tests/Orc.FilterBuilder.Tests.Shared/LinqExpressionFacts.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Orc.FilterBuilder.Models;
+using Orc.FilterBuilder.Conditions;
+using System.Linq.Expressions;
+
+namespace Orc.FilterBuilder.Tests
+{
+    [TestFixture]
+    public class LinqExpressionFacts
+    {
+        [TestCase]
+        public void Bool_ExpressionTest()
+        {
+            var items = CalcHumansFor("HasChailds", new BooleanExpression()
+            {
+                SelectedCondition = Condition.EqualTo,
+                Value = true 
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.HasChailds ).ToList()));
+
+             items = CalcHumansFor("Married", new BooleanExpression()
+            {
+                SelectedCondition = Condition.EqualTo,
+                Value = true
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Married.HasValue && t.Married.Value).ToList()));
+
+
+        }
+
+        [TestCase]
+        public void String_ExpressionTest()
+        {
+            var items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.Contains,
+                Value = "nn"
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Name != null && t.Name.Contains("nn")).ToList()));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.DoesNotContain,
+                Value = "nn"
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Name == null || !t.Name.Contains("nn")).ToList()));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.StartsWith,
+                Value = "A"
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Name != null && t.Name.StartsWith("A")).ToList()));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.DoesNotStartWith,
+                Value = "A"
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Name == null || !t.Name.StartsWith("A")).ToList()));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.EndsWith,
+                Value = "io"
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Name != null && t.Name.EndsWith("io")).ToList()));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.DoesNotEndWith,
+                Value = "io"
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Name == null || !t.Name.EndsWith("io")).ToList()));
+
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.EqualTo,
+                Value = "Ann"
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Name != null && t.Name == "Ann").ToList()));
+
+
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.NotEqualTo,
+                Value = "Sergio"
+            });
+            Assert.True(ListEquals(items, People.Where((t) => t.Name == null || t.Name != "Sergio").ToList()));
+
+
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.GreaterThan,
+                Value = "Ann"
+            });
+
+            Assert.True(ListEquals(
+                items,
+                People.Where((t) => string.Compare(t.Name, "Ann", StringComparison.InvariantCultureIgnoreCase) > 0).ToList()
+                ));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.GreaterThanOrEqualTo,
+                Value = "Ann"
+            });
+            Assert.True(ListEquals(
+               items,
+               People.Where((t) => string.Compare(t.Name, "Ann", StringComparison.InvariantCultureIgnoreCase) >= 0).ToList()
+               ));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.LessThan,
+                Value = "Sergio"
+            });
+            Assert.True(ListEquals(
+     items,
+     People.Where((t) => string.Compare(t.Name, "Sergio", StringComparison.InvariantCultureIgnoreCase) < 0).ToList()
+     ));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.LessThanOrEqualTo,
+                Value = "Sergio"
+            });
+            Assert.True(ListEquals(
+                    items,
+                    People.Where((t) => string.Compare(t.Name, "Sergio", StringComparison.InvariantCultureIgnoreCase) <= 0).ToList()
+            ));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.IsNull,
+            });
+            Assert.True(ListEquals(
+                    items,
+                    People.Where((t) => t.Name==null).ToList()
+            ));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.NotIsNull,
+            });
+            Assert.True(ListEquals(
+                    items,
+                    People.Where((t) => t.Name != null).ToList()
+            ));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.IsEmpty,
+            });
+            Assert.True(ListEquals(
+                    items,
+                  People.Where((t) => t.Name == string.Empty ).ToList()
+            ));
+
+            items = CalcHumansFor("Name", new StringExpression()
+            {
+                SelectedCondition = Condition.NotIsEmpty,
+            });
+            Assert.True(ListEquals(
+                    items,
+                   People.Where((t) => t.Name != string.Empty).ToList()
+            ));
+        }
+
+        bool ListEquals(IList<Human> m1, IList<Human> m2)
+        {
+            if (m1 == null || m2 == null) return false;
+            if (m1.Count != m2.Count) return false;
+            var c = m1.Count;
+            for (int i = 0; i < c; i++)
+            {
+                if (m1[i].Name != m2[i].Name)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        List<Human> CalcHumansFor(string propertyName, DataTypeExpression dataTypeExpression)
+
+        {
+            var conditionGroup = new ConditionGroup()
+            {
+                Type = ConditionGroupType.And
+            };
+
+            var type = typeof(Human);
+
+            var propertyExpression = new PropertyExpression()
+            {
+                Property = new PropertyMetadata(type, type.GetProperty(propertyName)),
+                DataTypeExpression = dataTypeExpression
+            };
+
+            conditionGroup.Items.Add(propertyExpression);
+
+            var expression = conditionGroup.MakeFunction<Human>();
+            var result = People.AsQueryable().Where(expression).ToList();
+            return result;
+        }
+        
+        public List<Human> People => new List<Human>()
+        {
+            new Human("Ann",5,false),
+            new Human("Sergio", 35, true ),
+            new Human("", 35, true ),
+            new Human(null, 35, true )
+
+        };
+
+        public class Human
+        {
+            public Human(string name, int age, bool hasChailds)
+            {
+                Name = name;
+                Age = age;
+                HasChailds = hasChailds;
+            }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public bool HasChailds { get; set; }
+            public bool? Married { get; set; }
+        }
+
+    }
+}
---
+++ src/Orc.FilterBuilder/Orc.FilterBuilder.Shared/Extensions/ConditionsLinqExtention.cs
@@ -0,0 +1,319 @@
+﻿using Catel;
+using Orc.FilterBuilder.Models;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Orc.FilterBuilder.Conditions
+{
+    public static class ConditionsLinqExtention
+    {
+
+        public static Expression<Func<T, bool>> MakeFunction<T>(this ConditionTreeItem conditionTreeItem)
+        {
+            var type = typeof(T);
+            var pe = Expression.Parameter(type, "item");
+            Expression expression = conditionTreeItem.MakeExpression(pe);
+            if (expression == null)
+            {
+                return null;
+            }
+            var lambda = Expression.Lambda<Func<T, bool>>(expression, pe);
+            return lambda;
+        }
+
+        private static Expression MakeExpression(this ConditionTreeItem conditionTreeItem, ParameterExpression parametr)
+        {
+            if (conditionTreeItem.GetType() == typeof(ConditionGroup))
+            {
+                return ((ConditionGroup)conditionTreeItem).MakeExpression(parametr);
+            }
+            else if (conditionTreeItem.GetType() == typeof(PropertyExpression))
+            {
+                return ((PropertyExpression)conditionTreeItem).MakeExpression(parametr);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        private static Expression MakeExpression(this ConditionGroup conditionGroup, ParameterExpression parametr)
+        {
+
+            if (!conditionGroup.Items.Any())
+            {
+                return null;
+            }
+
+
+            Expression final = null;
+            Expression left = null;
+            Expression rigth = null;
+            foreach (var item in conditionGroup.Items)
+            {
+                var curExp = item?.MakeExpression(parametr);
+                if (curExp == null) continue;
+                if (left == null)
+                {
+                    left = curExp;
+                }
+                else
+                {
+                    rigth = curExp;
+                    if (conditionGroup.Type == ConditionGroupType.And)
+                    {
+                        final = Expression.AndAlso(left, rigth);
+                    }
+                    else
+                    {
+                        final = Expression.OrElse(left, rigth);
+                    }
+                    left = final;
+                    rigth = null;
+                }
+            }
+
+            //return final != null ? final : left;
+            return final ?? left;
+        }
+
+        private static Expression MakeExpression(this PropertyExpression propertyExpression, ParameterExpression pe)
+        {
+            return propertyExpression.DataTypeExpression.MakeExpression(pe, propertyExpression.Property);
+        }
+
+        private static Expression MakeExpression(this DataTypeExpression dataTypeExpression, ParameterExpression pe, IPropertyMetadata propertyMetadata)
+        {
+
+            if (dataTypeExpression.GetType() == typeof(BooleanExpression))
+            {
+                return ((BooleanExpression)dataTypeExpression).MakeExpression(pe, propertyMetadata.Name);
+            }
+            else if (dataTypeExpression.GetType() == typeof(StringExpression))
+            {
+                return ((StringExpression)dataTypeExpression).MakeExpression(pe, propertyMetadata.Name);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static Expression MakeExpression(this BooleanExpression expression, ParameterExpression pe, string propertyName)
+        {
+            var Value = expression.Value;
+            var SelectedCondition = expression.SelectedCondition;
+            Expression notNullExp;
+            Expression e;
+            Expression final;
+            switch (SelectedCondition)
+            {
+                case Condition.EqualTo:
+                    notNullExp = Expression.Not(IsNullExpression(pe, propertyName));
+                    Expression left = PropertyExpression(pe, propertyName);
+                    Expression rigth = Expression.Constant(Value);
+                    e = Expression.Equal(left, rigth);
+                    final = Expression.AndAlso(notNullExp, e);
+                    return final;
+                default:
+                    throw new NotSupportedException(string.Format(LanguageHelper.GetString("FilterBuilder_Exception_Message_ConditionIsNotSupported_Pattern"), SelectedCondition));
+            }
+        }
+        private static Expression MakeExpression(this StringExpression expression, ParameterExpression pe, string propertyName)
+        {
+            var Value = expression.Value;
+            var SelectedCondition = expression.SelectedCondition;
+
+            Expression notNulExp;
+            Expression left;
+            Expression rigth;
+            Expression e;
+            Expression final;
+            switch (SelectedCondition)
+            {
+
+                case Condition.Contains:
+                    notNulExp = NotIsNullOrEmptyExpression(pe, propertyName);
+                    left = Expression.Property(pe, propertyName);
+                    rigth = Expression.Constant(Value);
+                    e = Expression.Call(left, typeof(string).GetMethod("Contains", new Type[] { typeof(string) }), rigth);
+                    final = Expression.AndAlso(notNulExp, e);
+                    return final;
+
+                case Condition.DoesNotContain:
+                    e = (new StringExpression()
+                    {
+                        SelectedCondition = Condition.Contains,
+                        Value = expression.Value
+                    }).MakeExpression(pe, propertyName);
+                    return Expression.Not(e);
+
+                case Condition.StartsWith:
+                    notNulExp = NotIsNullOrEmptyExpression(pe, propertyName);
+                    left = Expression.Property(pe, propertyName);
+                    rigth = Expression.Constant(Value);
+                    e = Expression.Call(left, typeof(string).GetMethod("StartsWith", new Type[] { typeof(string) }), rigth);
+                    final = Expression.AndAlso(notNulExp, e);
+                    return final;
+
+                case Condition.DoesNotStartWith:
+                    e = (new StringExpression()
+                    {
+                        SelectedCondition = Condition.StartsWith,
+                        Value = expression.Value
+                    }).MakeExpression(pe, propertyName);
+                    return Expression.Not(e);
+
+                case Condition.EndsWith:
+                    notNulExp = NotIsNullOrEmptyExpression(pe, propertyName);
+                    left = Expression.Property(pe, propertyName);
+                    rigth = Expression.Constant(Value);
+                    e = Expression.Call(left, typeof(string).GetMethod("EndsWith", new Type[] { typeof(string) }), rigth);
+                    final = Expression.AndAlso(notNulExp, e);
+                    return final;
+
+                case Condition.DoesNotEndWith:
+                    e = (new StringExpression()
+                    {
+                        SelectedCondition = Condition.EndsWith,
+                        Value = expression.Value
+                    }).MakeExpression(pe, propertyName);
+                    return Expression.Not(e);
+
+                case Condition.EqualTo:
+                    notNulExp = NotIsNullOrEmptyExpression(pe, propertyName);
+                    left = Expression.Property(pe, propertyName);
+                    rigth = Expression.Constant(Value);
+                    e = Expression.Equal(left, rigth);
+                    final = Expression.AndAlso(notNulExp, e);
+                    return final;
+
+                case Condition.NotEqualTo:
+                    e = (new StringExpression()
+                    {
+                        SelectedCondition = Condition.EqualTo,
+                        Value = expression.Value
+                    }).MakeExpression(pe, propertyName);
+                    return Expression.Not(e);
+
+                case Condition.GreaterThan:
+                    e = expression.CompareExpression(pe, propertyName);
+                    final = Expression.GreaterThan(e, Expression.Constant(0));
+                    return final;
+
+                case Condition.GreaterThanOrEqualTo:
+                    e = expression.CompareExpression(pe, propertyName);
+
+                    final = Expression.GreaterThanOrEqual(e, Expression.Constant(0));
+
+                    return final;
+
+                case Condition.LessThan:
+                    e = expression.CompareExpression(pe, propertyName);
+
+                    final = Expression.LessThan(e, Expression.Constant(0));
+
+                    return final;
+
+                case Condition.LessThanOrEqualTo:
+                    e = expression.CompareExpression(pe, propertyName);
+                    final = Expression.LessThanOrEqual(e, Expression.Constant(0));
+                    return final;
+
+                case Condition.IsNull:
+                    left = Expression.Property(pe, propertyName);
+                    rigth = Expression.Constant(null);
+                    e = Expression.Equal(left, rigth);
+                    return e;
+
+                case Condition.NotIsNull:
+                    left = Expression.Property(pe, propertyName);
+                    rigth = Expression.Constant(null);
+                    e = Expression.Equal(left, rigth);
+                    final = Expression.Not(e);
+                    return final;
+
+                case Condition.IsEmpty:
+                    left = Expression.Property(pe, propertyName);
+                    rigth = Expression.Constant(string.Empty);
+                    e = Expression.Equal(left, rigth);
+
+                    return e;
+                case Condition.NotIsEmpty:
+                    left = Expression.Property(pe, propertyName);
+                    rigth = Expression.Constant(string.Empty);
+                    e = Expression.Equal(left, rigth);
+                    final = Expression.Not(e);
+                    return final;
+
+                case Condition.Matches:
+                    return null;
+
+                case Condition.DoesNotMatch:
+                    return null;
+
+                default:
+                    throw new NotSupportedException(string.Format(LanguageHelper.GetString("FilterBuilder_Exception_Message_ConditionIsNotSupported_Pattern"), SelectedCondition));
+            }
+        }
+        private static Expression CompareExpression(this StringExpression expression, ParameterExpression pe, string propertyName)
+        {
+            var Value = expression.Value;
+            Expression left;
+            Expression rigth;
+            Expression e;
+            left = Expression.Property(pe, propertyName);
+            rigth = Expression.Constant(Value);
+            var method = typeof(string).GetMethod("Compare", new Type[] { typeof(string), typeof(string), typeof(StringComparison) });
+            e = Expression.Call(method, left, rigth, Expression.Constant(StringComparison.InvariantCultureIgnoreCase));
+            return e;
+        }
+
+        private static Expression IsNullOrEmptyExpression(ParameterExpression pe, string propertyName)
+        {
+            var method = typeof(string).GetMethod("IsNullOrEmpty", new Type[] { typeof(string) });
+            Expression arg = Expression.Property(pe, propertyName);
+            Expression e = Expression.Call(method, arg);
+            return e;
+        }
+        private static Expression NotIsNullOrEmptyExpression(ParameterExpression pe, string propertyName)
+        {
+            Expression e = IsNullOrEmptyExpression(pe, propertyName);
+            return Expression.Not(e);
+        }
+        private static Expression IsNullExpression(ParameterExpression pe, string propertyName)
+        {
+            var type = Expression.Property(pe, propertyName).Type;
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                Expression left;
+                Expression rigth;
+                Expression e;
+                left = Expression.Property(pe, propertyName);
+                rigth = Expression.Constant(null);
+                e = Expression.Equal(left, rigth);
+                return e;
+            }
+            else
+            {
+                return Expression.Constant(false);
+            }
+        }
+        private static Expression PropertyExpression(ParameterExpression pe, string propertyName)
+        {
+            var type = Expression.Property(pe, propertyName).Type;
+            if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+            {
+                var prop = Expression.Property(pe, propertyName);
+                var propValue = Expression.Property(prop, "Value");
+                return propValue;
+            }
+            else
+            {
+                return Expression.Property(pe, propertyName);
+            }
+        }
+    }
+}
