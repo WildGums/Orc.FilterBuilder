0eedfe74100de833f703e34e626b9042da1d7c87
Geert van Horrik <geert@catenalogic.com>
2019-02-12 13:22:46 +0100

    Add unit test for #56

---
+++ src/Orc.FilterBuilder.Tests/Converters/ObjectToValueConverterFacts.cs
@@ -0,0 +1,31 @@
+﻿namespace Orc.FilterBuilder.Tests.Converters
+{
+    using Catel.Data;
+    using NUnit.Framework;
+    using Orc.FilterBuilder.Converters;
+
+    [TestFixture]
+    public class ObjectToValueConverterFacts
+    {
+        private class TestModel : ModelBase
+        {
+            public string Reference { get => "test"; }
+
+            public int MyIntegerValue { get; set; }
+        }
+
+        [TestCase]
+        public void ConvertsGetterPropertiesFromCatelModelBase()
+        {
+            var model = new TestModel
+            {
+                MyIntegerValue = 42
+            };
+
+            var converter = new ObjectToValueConverter();
+
+            Assert.AreEqual(42, converter.Convert(model, null, nameof(TestModel.MyIntegerValue), null));
+            Assert.AreEqual("test", converter.Convert(model, null, nameof(TestModel.Reference), null));
+        }
+    }
+}
