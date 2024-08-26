7c2969486791735fa7795afe90f58dec2140749c
Vladimir <work.lvg@gmail.com>
2024-08-26 10:47:14 +0300

    fix first chance exceptnions

---
+++ src/Orc.FilterBuilder.Xaml/Converters/EnsureDateTimeValueConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace Orc.FilterBuilder.Converters;
+
+using System;
+using Catel.MVVM.Converters;
+
+internal class EnsureDateTimeValueConverter : ValueConverterBase<object, DateTime?>
+{
+    protected override object? Convert(object? value, Type targetType, object? parameter)
+    {
+        if (value is not DateTime time)
+        {
+            return null;
+        }
+
+        return time;
+    }
+}
---
+++ src/Orc.FilterBuilder.Xaml/Converters/EnsureTimeSpanValueConverter.cs
@@ -0,0 +1,17 @@
+﻿namespace Orc.FilterBuilder.Converters;
+
+using System;
+using Catel.MVVM.Converters;
+
+internal class EnsureTimeSpanValueConverter : ValueConverterBase<object, DateTime?>
+{
+    protected override object? Convert(object? value, Type targetType, object? parameter)
+    {
+        if (value is not TimeSpan time)
+        {
+            return null;
+        }
+
+        return time;
+    }
+}
