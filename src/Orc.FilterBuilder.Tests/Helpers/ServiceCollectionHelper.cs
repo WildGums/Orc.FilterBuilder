1fc6553c00a0b2fc433d21e2bbe7201b9990e36d
Geert van Horrik <geert@catenalogic.com>
2026-04-08 09:26:27 +0200

    Convert to file scoped namespaces

--- src/Orc.FilterBuilder.Tests/Helpers/ServiceCollectionHelper.cs
+++ src/Orc.FilterBuilder.Tests/Helpers/ServiceCollectionHelper.cs
@@ -1,24 +1,23 @@
-﻿namespace Orc.FilterBuilder.Tests
+﻿namespace Orc.FilterBuilder.Tests;
+
+using Catel;
+using Microsoft.Extensions.DependencyInjection;
+using Orc.FileSystem;
+using Orc.Serialization.Json;
+
+internal static class ServiceCollectionHelper
 {
-    using Catel;
-    using Microsoft.Extensions.DependencyInjection;
-    using Orc.FileSystem;
-    using Orc.Serialization.Json;
-
-    internal static class ServiceCollectionHelper
+    public static IServiceCollection CreateServiceCollection()
     {
-        public static IServiceCollection CreateServiceCollection()
-        {
-            var serviceCollection = new ServiceCollection();
+        var serviceCollection = new ServiceCollection();
 
-            serviceCollection.AddLogging();
-            serviceCollection.AddCatelCore();
-            serviceCollection.AddOrcFileSystem();
-            serviceCollection.AddOrcFilterBuilder();
-            serviceCollection.AddOrcFilterBuilderXaml();
-            serviceCollection.AddOrcSerializationJson();
+        serviceCollection.AddLogging();
+        serviceCollection.AddCatelCore();
+        serviceCollection.AddOrcFileSystem();
+        serviceCollection.AddOrcFilterBuilder();
+        serviceCollection.AddOrcFilterBuilderXaml();
+        serviceCollection.AddOrcSerializationJson();
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
--- src/Orc.FilterBuilder/OrcFilterBuilderModule.cs
+++ src/Orc.FilterBuilder/OrcFilterBuilderModule.cs
@@ -1,29 +1,28 @@
-﻿namespace Orc
+﻿namespace Orc;
+
+using Catel.Services;
+using Catel.ThirdPartyNotices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Orc.FilterBuilder;
+
+/// <summary>
+/// Core module which allows the registration of default services in the service collection.
+/// </summary>
+public static class OrcFilterBuilderModule
 {
-    using Catel.Services;
-    using Catel.ThirdPartyNotices;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
-    using Orc.FilterBuilder;
-
-    /// <summary>
-    /// Core module which allows the registration of default services in the service collection.
-    /// </summary>
-    public static class OrcFilterBuilderModule
+    public static IServiceCollection AddOrcFilterBuilder(this IServiceCollection serviceCollection)
     {
-        public static IServiceCollection AddOrcFilterBuilder(this IServiceCollection serviceCollection)
-        {
-            serviceCollection.TryAddSingleton<IFilterService, FilterService>();
-            serviceCollection.TryAddSingleton<IReflectionService, ReflectionService>();
-            serviceCollection.TryAddSingleton<IFilterSchemeManager, FilterSchemeManager>();
-            serviceCollection.TryAddSingleton<IFilterCustomizationService, FilterCustomizationService>();
-            serviceCollection.TryAddSingleton<IFilterSerializationService, FilterSerializationService>();
+        serviceCollection.TryAddSingleton<IFilterService, FilterService>();
+        serviceCollection.TryAddSingleton<IReflectionService, ReflectionService>();
+        serviceCollection.TryAddSingleton<IFilterSchemeManager, FilterSchemeManager>();
+        serviceCollection.TryAddSingleton<IFilterCustomizationService, FilterCustomizationService>();
+        serviceCollection.TryAddSingleton<IFilterSerializationService, FilterSerializationService>();
 
-            serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.FilterBuilder", "Orc.FilterBuilder.Properties", "Resources"));
+        serviceCollection.AddSingleton<ILanguageSource>(new LanguageResourceSource("Orc.FilterBuilder", "Orc.FilterBuilder.Properties", "Resources"));
 
-            serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.FilterBuilder", "https://github.com/wildgums/orc.filterbuilder"));
+        serviceCollection.AddSingleton<IThirdPartyNotice>((x) => new LibraryThirdPartyNotice("Orc.FilterBuilder", "https://github.com/wildgums/orc.filterbuilder"));
 
-            return serviceCollection;
-        }
+        return serviceCollection;
     }
 }
